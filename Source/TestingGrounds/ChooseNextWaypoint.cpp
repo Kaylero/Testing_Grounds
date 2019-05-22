f75ae71cc01c30ec272f29e04499e86e53ce8fd9
Pedro Calero <kalerolloko@gmail.com>
2019-05-22 12:22:11 +0200

    282. Composition over inheritance

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -2,12 +2,32 @@
 
 #include "ChooseNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "PatrolRoute.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
-	
-	UE_LOG(LogTemp, Warning, TEXT("Waypoint index: %i"), Index);
+
+	auto AIController = OwnerComp.GetAIOwner();
+	if (!ensure(AIController)) { return EBTNodeResult::Failed; }
+
+	auto Pawn = AIController->GetPawn();
+	if (!ensure(Pawn)) { return EBTNodeResult::Failed; }
+
+	auto PatrolRouteComponent = Pawn->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(PatrolRouteComponent)) { return EBTNodeResult::Failed; }
+
+	auto RoutePoints = PatrolRouteComponent->GetRoutePoints();
+
+	if (RoutePoints.Num() == 0) { return EBTNodeResult::Failed; }
+
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, RoutePoints[Index]);
+
+	Index++;
+	Index = Index % RoutePoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, Index);
+
 	return EBTNodeResult::Succeeded;
 }
